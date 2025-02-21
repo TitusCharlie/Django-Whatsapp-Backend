cf6345aafb8d2653865bf58b78129a0d1492b315
titus charlie <tituslifecharlie@gmail.com>
2025-02-22 00:37:47 +0100

    initial commit for Render deployment"

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,17 +109,19 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+    #'default': {
+        #'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': 'whatsapp_db',
+        #'USER': 'tituscharlie',
+        #'PASSWORD': '2427543Ediang',
+        #'HOST': 'localhost',
+        #'PORT': '5432',
+    #}
+#}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'whatsapp_db',
-        'USER': 'tituscharlie',
-        'PASSWORD': '2427543Ediang',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
