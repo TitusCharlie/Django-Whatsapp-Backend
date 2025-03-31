2bae499979cec5eaf9c34cc1b1440c8647ab2678
titus charlie <tituslifecharlie@gmail.com>
2025-04-01 00:36:18 +0100

    updated the modelsd and views

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import WhatsAppMessage, Contact, Group
+from .models import WhatsAppMessage, Contact, WhatsAppGroup
 
 admin.site.register(WhatsAppMessage)
 admin.site.register(Contact)
-admin.site.register(Group)
+admin.site.register(WhatsAppGroup)
